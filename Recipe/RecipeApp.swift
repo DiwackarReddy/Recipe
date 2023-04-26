25d42ea7337b569c54027c7c4ce78a7aa233f66d
Mohan <mohan@hansaitechnosoft.com>
2023-04-26 13:01:51 +0530

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Diwakar Reddy  on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
